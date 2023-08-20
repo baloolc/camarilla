478b18e40c12ff3465b325b4822a8efc4128dd42
vianney moreau <viann45@hotmail.fr>
2023-08-20 19:20:31 +0200

    creation de du form type characterRequest + création du controller CharacterRequestController + création du mail character request

---
+++ src/Controller/CharacterRequestController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CharacterRequest;
+use App\Form\CharacterRequestType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CharacterRequestController extends AbstractController
+{
+    #[Route('/request', name: 'character_request')]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        /** @var User  */
+        $user = $this->getUser();
+        $characterRequest = new CharacterRequest();
+        $form = $this->createForm(CharacterRequestType::class, $characterRequest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $email = (new Email())
+                ->from($user->getEmail())
+                ->to($this->getParameter('mailer_story'))
+                ->subject($user->getFirstname() . ' ' . $user->getLastname() . ' fait la demande d\'un nouveau personnage')
+                ->html($this->renderView('email/email_character_request.html.twig', [
+                    'characterRequest' => $characterRequest,
+                    'user' => $user,
+            ]));
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre email est bien envoyé');
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('character_request/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/CharacterRequestType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CharacterRequest;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CharacterRequestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('characterName', TextType::class, [
+                'label' => 'Nom du personnage',
+                'required' => 'true',
+                'attr' => [
+                    'placeholder' => 'Golum',
+                ]
+            ])
+            ->add('clan', ChoiceType::class, [
+                'label' => 'Nom',
+                'required' => 'true',
+                'choices' => [
+                    'Nosferatu' => 'Nosferatu',
+                    'Ventrue' => 'Ventrue',
+                    'Gangrel' => 'Gangrel',
+                    'Brujah' => 'Brujah',
+                    'Toréador' => 'Toréador',
+                    'Malkavian' => 'Malkavian',
+                    'Trémère' => 'Trémère',
+                    'Ravnos' => 'Ravnos',
+                    'Giovani' => 'Giovani',
+                    'Assamite' => 'Assamite',
+                    'Settite' => 'Settite',
+                    'Lasombra' => 'Lasombra',
+                    'Caïtif' => 'Caïtif',
+                    'Tzimisce' => 'Tzimisce',
+                ],
+            ])
+            ->add('secte', ChoiceType::class, [
+                'label' => 'Secte',
+                'required' => 'true',
+                'choices' => [
+                    'Camarilla' => 'Camarilla',
+                    'Anarch' => 'Anarch',
+                    'Sabbat' => 'Sabbat',
+                    'Aubain' => 'Aubain',
+                ],
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Statut d\'âge',
+                'required' => 'true',
+                'choices' => [
+                    'Infant sous tutelle' => 'Infant sous tutelle',
+                    'Nouveau née' => 'Nouveau née',
+                    'Ancilla' => 'Ancilla',
+                    'Ancien' => 'Ancien',
+                    'Vénérable' => 'Vénérable',
+                    'Aucun' => 'Aucun',
+                ],
+            ])
+            ->add('pattern', TextareaType::class, [
+                'label' => 'Motif de la demande',
+                'required' => 'true',
+                'attr' => [
+                    'placeholder' => 'Mettez votre motif ici !',
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CharacterRequest::class,
+        ]);
+    }
+}
